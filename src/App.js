25073a7a783e036ee9da390c3fb44940a108f8bd
nguyenfamj <nguyenfamj1409@gmail.com>
2022-02-21 15:22:04 +0200

    added react-parallax-tilt.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Card from './Components/Card/Card';
+import Tilt from 'react-parallax-tilt';
 
 function App() {
   return (
     <div
-      className="flex items-center w-screen h-screen bg-cover App"
+      className="flex items-center justify-center w-screen h-screen bg-cover App"
       style={{
         backgroundImage: `url("https://wallpapersmug.com/large/3bcbc6/digital-art-neon-bubbles.jpg")`,
       }}
     >
-      <Card></Card>
+      <Tilt tiltReverse={true}>
+        <Card></Card>
+      </Tilt>
     </div>
   );
 }
